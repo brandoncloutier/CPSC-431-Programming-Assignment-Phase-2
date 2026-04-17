05d144306ab6059c47cbfde6de2b0ea085f7c279
chrishernzz <christianhernandez1014@gmail.com>
2026-04-16 18:17:48 -0700

    User can select by Id now.

--- public/app.js
+++ public/app.js
@@ -7,12 +7,14 @@ async function fetchAllLists() {
     const lists = await response.json()
     
     const container = document.getElementById("listscontainer")
-    container.innerHTML = " "
+    container.innerHTML = ""
 
     //add the data to the listcontainer
     lists.forEach(list => {
         const div = document.createElement("div")
         div.textContent = list.title
+        //pass in to the func parameter if user clicks on a certain list 
+        div.onclick = () => fetchById(list._id.toString())
         container.appendChild(div)
     })
 }
@@ -41,6 +43,32 @@ async function createList() {
     //once user is done typing put it back to empty
     input.value = ""
     fetchAllLists()
+}
+
+//precondition: takes in parameter to retrieve from Db
+//postcondition: returns a single list
+async function fetchById(id) {
+    //TEST: if clicked should show
+    console.log("clicked list id:",id)
+
+
+    //call the lists to only get the Id
+    const response = await fetch(`/api/lists/${id}`)
+    const list = await response.json()
+
+    //call the title from the Id
+    document.getElementById("selectedlisttitle").textContent = list.title
+
+    const entriesContainer = document.getElementById("entriescontainer")
+    entriesContainer.innerHTML = ""
+
+    list.entries.forEach(entry => {
+        const div = document.createElement("div")
+        div.textContent = entry.text
+        entriesContainer.appendChild(div)
+    })
+
+    document.getElementById("listdetail").style.display = "block"
 }
 
 document.getElementById("createlistbutton").addEventListener("click",createList)
